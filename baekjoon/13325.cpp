c6742734da1aafda083f2a390c613417d491a93a
sanghyeon <tjtkdgus45@nate.com>
2022-01-19 14:40:29 +0900

    algorithm 22.01.19

---
+++ baekjoon/13325.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int MK = 21;
+int tree[2 << (MK + 1)];
+int K;
+
+int dfs(int idx) {
+    if(idx * 2 + 2 > pow(2, K + 1) - 1) return tree[idx];
+    int left = dfs(idx * 2 + 1);
+    int right = dfs(idx * 2 + 2);
+
+    int gap = abs(left- right);
+    if(left < right)
+        tree[idx * 2 + 1] += gap;
+    else
+        tree[idx * 2 + 2] += gap;
+
+    return max(left, right) + tree[idx];
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cin >> K;
+    for(int i = 1; i <= pow(2, K + 1) - 2; i++)
+        cin >> tree[i];
+
+    dfs(0);
+
+    int ans = 0;
+
+    for(int i = 1; i <= pow(2, K + 1) - 1; i++)
+        ans += tree[i];
+
+    cout << ans;
+}
