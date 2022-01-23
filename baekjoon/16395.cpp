f2f2c43d441494d1057c6e2d9ea99f239d125050
sanghyeon <tjtkdgus45@nate.com>
2022-01-23 12:17:30 +0900

    algorithm 22.01.23

---
+++ baekjoon/16395.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+const int MN = 1001;
+int arr[MN][MN];
+
+int main() {
+    int N, K; cin >> N >> K;
+    arr[0][0] = 1;
+    for(int i = 1; i <= N; i++) {
+        arr[i][0] = 1;
+        for(int j = 1; j < i; j++) 
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+        arr[i][i] = 1;
+    }
+    cout << arr[N-1][K-1];
+}
