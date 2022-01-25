a7156acd7830f5b8614bd4088e935514ae7dbd74
Seo-sang <tjtkdgus45@nate.com>
2022-01-25 17:22:19 +0900

    algorithm 22.01.25

---
+++ baekjoon/1208.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int MN = 41;
+int arr[MN];
+
+int main() {
+    int N, S; cin >> N >> S;
+    for(int i = 0; i < N; i++)
+        cin >> arr[i];
+}
---
+++ baekjoon/2776.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int MN = 1e6 + 1;
+int arr[MN];
+
+int main() {
+    int T; cin >> T;
+    while(T--) {
+        int N; cin >> N;
+        for(int i = 0; i < N; i++)
+            cin >> arr[i];
+
+        sort(arr, arr + N);
+        int M; cin >> M;
+        while(M--) {
+            int a; cin >> a;
+            cout << binary_search(arr, arr + N, a) << '\n';
+        }
+    }
+}
