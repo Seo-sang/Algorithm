38ff86794070f713939e84f28d3c4bdc3b77515e
sanghyeon <tjtkdgus45@nate.com>
2022-01-26 20:33:20 +0900

    algorithm 22.01.26

--- baekjoon/1561.cpp
+++ baekjoon/1561.cpp
@@ -9,31 +9,36 @@ int N, M;
 ll f(int mid) {
     ll ret = 0;
     for(int i = 0; i < M; i++)
-        ret += (arr[i] / mid);
+        ret += (mid / arr[i]);
 
     return ret;
 }
 int main() {
+
     cin >> N >> M;
-    int lo = 31, hi;
+    int lo = 31, hi = 0;
     for(int i = 0; i < M; i++) {
         cin >> arr[i];
         lo = min(lo, arr[i]);
+        hi = max(hi, arr[i]);
     }
     lo--;
-    hi = lo * N;
+    hi = hi * N;
 
     while(lo + 1 < hi) {
         int mid = (lo + hi) / 2;
         ll ret = f(mid);
+        //cout << mid << ' ' << ret << endl;
         if(ret < N - 1) lo = mid;
         else hi = mid;
     }
-    cout << hi << endl;
 
-    for(int i = 0; i < M; i++) {
-        if(arr[i] % hi == 0) {
-            cout << i + 1;
+    cout << hi << endl;
+    bool chk = false;
+    for(int i = M -1; i >= 0; i--) {
+        cout << N << ' ' << hi / arr[i] << endl;
+        if(hi % arr[i] == 0) {
+            cout << (i + 1) % M + 1;
             break;
         }
     }
