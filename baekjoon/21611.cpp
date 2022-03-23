294df518b5f4d4a08e73555fec28d091a4f6b397
sanghyeon <tjtkdgus45@nate.com>
2022-03-24 01:12:54 +0900

    algorithm 22.03.25

--- baekjoon/21611.cpp
+++ baekjoon/21611.cpp
@@ -12,24 +12,88 @@ int dy[4] = {0, 0, -1, 1};
 int cx[4] = {0, 1, 0, -1};
 int cy[4] = {-1, 0, 1, 0};
 
-void destroy(int x, int y) {
-    switch(ball[idx[x][y]]) {
+void destroy(int color, int n) {
+    switch(color) {
         case 1:
-            ball1++;
+            ball1 += n;
             break;
         case 2:
-            ball2++;
+            ball2 += n;
             break;
         case 3:
-            ball3++;
+            ball3 += n;
             break;
     }
-    ball[idx[x][y]] = 0;
+}
+
+//빈 칸 없애는 함수
+void compact() {
+    vector<int> rst;
+    rst.push_back(0);
+    for(int i = 1; i < ball.size(); i++)
+        if(ball[i] != 0)
+            rst.push_back(ball[i]);
+
+    while(rst.size() < N * N)
+        rst.push_back(0);
+    ball = rst;
+}
+
+void explode() {
+    vector<int> rst;
+    rst.push_back(0);
+    for(int i = 1; i < ball.size(); i++) {
+        if(rst.size() < 4) rst.push_back(ball[i]);
+        else {
+            int idx = rst.size() - 1;
+            if(ball[i] == rst[idx] && rst[idx] == rst[idx-1] && rst[idx-1] == rst[idx-2]) {
+                for(int p = 0; p < 3; p++)
+                    rst.pop_back();
+                int n = 3;
+                int j = i + 1;
+                while(j < ball.size() && ball[i] == ball[j])
+                    j++;
+                j--;
+                n += (j - i + 1);
+                i = j;
+                destroy(ball[i], n);
+            }
+            else
+                rst.push_back(ball[i]);
+        }
+    }
+    while(rst.size() < N * N)
+        rst.push_back(0);
+    ball = rst;
+}
+
+void change() {
+    vector<int> rst;
+    rst.push_back(0);
+    for(int i = 1; i < ball.size(); i++) {
+        int j = i + 1;
+        int cnt = 1;
+        while(j < ball.size() && ball[i] == ball[j]) {
+            j++;
+            cnt++;
+        }
+        j--;
+        rst.push_back(cnt);
+        if(rst.size() == N * N) break;
+        rst.push_back(ball[i]);
+        if(rst.size() == N * N) break;
+        i = j;
+    }
+
+    while(rst.size() < N * N)
+        rst.push_back(0);
+    ball = rst;
 }
 
 int main() {
     cin >> N >> M;
-    ball.resize(N * N);
+    ball.resize(N * N, 0);
+    
     int num = 0, c = 0;
     P pos = make_pair(N / 2, N / 2);
     for(int len = 1; len < 50; len++) {
@@ -54,9 +118,22 @@ int main() {
     while(M--) {
         int d, s; cin >> d >> s; d--;
         int x = N / 2, y = N / 2;
+
+        //한 방향으로 구슬 파괴
         while(s--) {
             x += dx[d], y += dy[d];
-            destroy(x, y);
+            if(idx[x][y] < ball.size())
+                ball[idx[x][y]] = 0;
         }
+        //빈칸으로 이동
+        compact();
+
+        //구슬 폭발
+        explode();
+
+        //변경
+        change();
     }
+
+    cout << ball1 + (2 * ball2) + (3 * ball3);
 }
