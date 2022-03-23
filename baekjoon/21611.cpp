0485961949e5cf54db77144ede66bfaa688e8e6f
Seo-sang <tjtkdgus45@nate.com>
2022-03-23 16:55:00 +0900

    algorithm 22.03.23

--- baekjoon/21611.cpp
+++ baekjoon/21611.cpp
@@ -5,14 +5,31 @@ const int MN = 50;
 int board[MN][MN];
 int idx[MN][MN];
 int N, M;
+int ball1, ball2, ball3;
+vector<int> ball;
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 int cx[4] = {0, 1, 0, -1};
 int cy[4] = {-1, 0, 1, 0};
 
+void destroy(int x, int y) {
+    switch(ball[idx[x][y]]) {
+        case 1:
+            ball1++;
+            break;
+        case 2:
+            ball2++;
+            break;
+        case 3:
+            ball3++;
+            break;
+    }
+    ball[idx[x][y]] = 0;
+}
+
 int main() {
     cin >> N >> M;
-    vector<int> ball(N * N);
+    ball.resize(N * N);
     int num = 0, c = 0;
     P pos = make_pair(N / 2, N / 2);
     for(int len = 1; len < 50; len++) {
@@ -30,16 +47,16 @@ int main() {
 
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
-            cin >> board[i][j];
-            ball[idx[i][j]] = board[i][j];
+            int a; cin >> a;
+            ball[idx[i][j]] = a;
         }
     }
-
     while(M--) {
         int d, s; cin >> d >> s; d--;
         int x = N / 2, y = N / 2;
         while(s--) {
-            
+            x += dx[d], y += dy[d];
+            destroy(x, y);
         }
     }
 }
