251ae0066ff92b3caadd4420635984cee6993dfd
sanghyeon <tjtkdgus45@nate.com>
2022-04-04 21:31:35 +0900

    algorithm 22.04.04

--- baekjoon/18500.cpp
+++ baekjoon/18500.cpp
@@ -5,12 +5,9 @@ using namespace std;
 
 const int MN = 101;
 string board[MN];
-string nxt[MN];
-bool vst[MN][MN];
-int bot[MN];
+bool vst[MN][MN], group[MN][MN];
 int dx[4] = {1, 0, 0, -1};
 int dy[4] = {0, 1, -1, 0};
-
 int R, C;
 
 void LtoR(int level) {
@@ -40,18 +37,15 @@ void drop() {
     for(int i = 0; i < R; i++) {
         for(int j = 0; j < C; j++) {
             if(board[i][j] == '.' || vst[i][j]) continue;
-            memset(bot, -1, sizeof(bot));
-            bool above = true;
+            memset(group, 0, sizeof(group));
             vst[i][j] = true;
+            group[i][j] = true;
             vector<P> v;
             queue<P> q;
             q.push(make_pair(i, j));
             v.push_back(make_pair(i, j));
-            //cout << "-----------------\n";
-            //cout << i << ' ' << j << ' ' << board[i][j] << endl;
             while(!q.empty()) {
                 P now = q.front(); q.pop();
-                bot[now.second] = max(bot[now.second], now.first);
                 for(int d = 0; d < 4; d++) {
                     int nx = now.first + dx[d];
                     int ny = now.second + dy[d];
@@ -59,32 +53,32 @@ void drop() {
                     if(nx >= 0 && nx < R && ny >= 0 && ny < C) {
                         if(board[nx][ny] == '.' || vst[nx][ny]) continue;
                         vst[nx][ny] = true;
+                        group[nx][ny] = true;
                         v.push_back(make_pair(nx, ny));
-                        //cout << nx << ' ' << ny << endl;
                         q.push(make_pair(nx, ny));
                     }
                 }
             }
             int depth = MN;
-            for(int c = 0; c < C; c++) {
-                if(bot[c] == -1) continue;
-                int idx = bot[c] + 1;
-                if(idx >= R) { //바닥과 닿는 경우
+            bool above = true;
+            for(P e : v) {
+                int idx = e.first + 1;
+                if(idx == R) {
                     above = false;
                     break;
                 }
+                if(board[idx][e.second] == 'x') continue;
                 int tmp = 0;
-                while(idx < R && board[idx][c] =='.') {
+                while((idx < R && board[idx][e.second] == '.') || (idx < R && group[idx][e.second])) {
                     tmp++;
                     idx++;
                 }
                 depth = min(depth, tmp);
             }
+            //떠있는 경우
             if(above) {
-                //cout <<"above : " << i << ' ' << j << '\n';
-                sort(v.begin(), v.end(), cmp);
+                sort(v.begin(), v.end(), cmp); //아래부터 차례대로 내림
                 for(P e : v) {
-                    //cout << e.first << ' ' << e.second << '\n';
                     board[e.first + depth][e.second] = 'x';
                     board[e.first][e.second] = '.';
                     vst[e.first+ depth][e.second] = true;
@@ -96,7 +90,7 @@ void drop() {
 }
 
 void print() {
-    for(int i = 0; i < R; i++)
+    for(int i = 0; i < R ; i++)
         cout << board[i] << '\n';
 }
 
