bb1ef2f832f35a77f9675362ec2d5376bc9af9bf
Seo-sang <tjtkdgus45@nate.com>
2022-04-07 23:38:44 +0900

    algorithm 22.04.07

---
+++ baekjoon/10875.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int L, N; cin >> L >> N;
+    for(int i = 0; i < N; i++) {
+        int t; char dir; cin >> t >> dir;
+    }
+}
