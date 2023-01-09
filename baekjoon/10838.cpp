03a8addf68571c9bb0a1c4d84a892dab8276b26b
sanghyeon <tjtkdgus45@nate.com>
2023-01-09 21:53:21 +0900

    -

---
+++ baekjoon/10838.cpp
@@ -0,0 +1,21 @@
+#include <bits.stdc++.h>
+
+using namespace std;
+
+const int MN = 100001;
+
+int main() {
+    int N, K; cin >> N >> K;
+    while(K--) {
+        int r, a, b, c; cin >> r;
+        if(r == 1) {
+            cin >> a >> b >> c;
+        }
+        else if(r == 2) {
+
+        }
+        else {
+
+        }
+    }
+}
