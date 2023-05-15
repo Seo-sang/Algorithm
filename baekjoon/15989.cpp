f61519f0c5d2702b34b9926680c2fe64a88caddb
Seo-sang <tjtkdgus45@nate.com>
2023-05-15 18:26:00 +0900

    -

--- baekjoon/15989.cpp
+++ baekjoon/15989.cpp
@@ -3,21 +3,22 @@
 using namespace std;
 
 const int MN = 10001;
-int dp[MN];
+const int INF = 1e9;
+int dp[MN][4];
 
 int main() {
     int T; cin >> T;
-
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 3;
+    memset(dp, INF, sizeof(dp));
+    dp[1][1] = dp[2][1] = dp[2][2] = dp[3][1] = dp[3][2] = dp[3][3] = 1;
 
     for(int i = 4; i < MN; i++) {
-        dp[i] = dp[i-1] + dp[i-2] - 1;
+        dp[i][1] = dp[i-1][1];
+        dp[i][2] = dp[i-2][1] + dp[i-2][2];
+        dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
     }
 
     while(T--) {
         int N; cin >> N;
-        cout << dp[N] << '\n';
+        cout << dp[N][1] + dp[N][2] + dp[N][3] << '\n';
     }
 }
