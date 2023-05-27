1000b59b209e02bc106ca33c7887498d2afc4015
Seo-sang <tjtkdgus45@nate.com>
2023-05-27 18:50:56 +0900

    -

---
+++ baekjoon/19622.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MN = 100001;
+
+struct node {
+    int start, end, num;
+    node(int a, int b, int c) : start(a), end(b), num(c) {}
+};
+
+vector<node> arr;
+int dp[MN][2];
+
+int main() {
+    int N; cin >> N;
+    arr.push_back(node(0, 0, 0));
+    for(int i = 0; i < N; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        arr.push_back(node(a, b, c));
+    }
+
+    for(int i = 1; i <= N; i++) {
+        node now = arr[i];
+        dp[i][1] = dp[i-1][0] + now.num;
+
+        if(arr[i].start == arr[i-1].end)
+            dp[i][1] = max(dp[i][1], dp[i-1][1] + now.num);
+        
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1]);
+    }
+
+    cout << max(dp[N][0], dp[N][1]);
+}
--- baekjoon/19623.cpp
+++ baekjoon/19623.cpp
@@ -1,50 +1,37 @@
 #include <bits/stdc++.h>
+#define pii pair<int,int>
 
 using namespace std;
 
 const int MN = 100001;
 
-struct node {
-    int start, end, num;
-    node(int a, int b, int c) : start(a), end(b), num(c) {}
-};
-
-bool cmp(node a, node b) {
-    return (a.start < b.start) || (a.start == b.start && a.end < b.end);
-}
-
-vector<node> arr;
 vector<int> v;  //압축 좌표
-set<int> st;
+map<int, vector<pii> > m;
 int dp[MN * 2];
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL);
     int N; cin >> N;
+
     for(int i = 0; i < N; i++) {
         int a, b, c; cin >> a >> b >> c;
-        arr.push_back(node(a, b, c));
         v.push_back(a);
         v.push_back(b);
+        m[a].push_back(pii(b, c));
     }
 
-    sort(arr.begin(), arr.end(), cmp);
     sort(v.begin(), v.end());
     v.erase(unique(v.begin(), v.end()), v.end());
     
-    st.insert(0);
-
     int ans = 0;
-    for(int i = 0; i < N; i++) {
-        int start, end ,num;
-        start = lower_bound(v.begin(), v.end(), arr[i].start) - v.begin();
-        end = lower_bound(v.begin(), v.end(), arr[i].end) - v.begin();
-        num = arr[i].num;
-        auto it = upper_bound(st.begin(), st.end(), start);
-        it--;
-        dp[end] = max(dp[end], dp[*it] + num);
-        ans = max(ans, dp[end]);
-        st.insert(end);
+    for(int i = 0; i < v.size(); i++) {
+        if(i > 0)
+            dp[i] = max(dp[i], dp[i-1]);
+        for(pii e : m[v[i]]) {
+            int end = lower_bound(v.begin(), v.end(), e.first) - v.begin();
+            dp[end] = max(dp[end], dp[i] + e.second);
+            ans = max(ans, dp[end]);
+        }
     }
 
     cout << ans;
