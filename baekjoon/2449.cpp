a06f6643706235bf29bc12026010773b79c40be4
Seo-sang <tjtkdgus45@nate.com>
2024-05-01 19:42:42 +0900

    -

--- baekjoon/2449.cpp
+++ baekjoon/2449.cpp
@@ -9,7 +9,7 @@ const int INF = 1e9;
 int dp[MN][MK];
 vector<int> arr;
 
-int dfs(int n, int k, int d) {
+int dfs(int n, int k, vector<int> &arr) {
 
     //left
     for(int i = n; i >= 0; i--) {
@@ -23,6 +23,8 @@ int main() {
     int N, K; cin >> N >> K;
 
     
+    fill(&dp[0][0], &dp[N-1][K], INF);
+
     for(int i =0;  i < N; i++) {
         int a; cin >> a;
         arr.push_back(a);
@@ -30,14 +32,9 @@ int main() {
     }
 
     for(int i = 0; i < N; i++) {
-        fill(&dp[0][0], &dp[N-1][K], INF);
-
-        dp[i][arr[i]] = 0;
-        dp[i][arr[i]][1] = 1;
-
         if(i > 0 && arr[i] != arr[i - 1]) {
-            dp[i - 1][arr[i]][0] = 1;
-            dfs(i - 1, arr[i], 0);
+            dp[i - 1][arr[i]] = 1;
+            dfs(i - 1, arr[i]);
         }
         if(i < N -1 && arr[i] != arr[i + 1]){
             dp[i + 1][arr[i]][1] = 1;
